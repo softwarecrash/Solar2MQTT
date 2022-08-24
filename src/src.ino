ce0762fcea9051610d95ee309091a27159b54cf2
Softwarecrash <lacrimosa2k2@gmail.com>
2022-08-25 00:05:33 +0200

    correct includes

--- src/src.ino
+++ src/src.ino
@@ -17,7 +17,7 @@
 #include "inverter.h"
 #include "Settings.h"
 
-#include "webpages/HTMLcase.h"     //The HTML Konstructor
+#include "webpages/HtmlCase.h"     //The HTML Konstructor
 #include "webpages/main.h"         //landing page with menu
 #include "webpages/settings.h"     //settings page
 #include "webpages/settingsedit.h" //mqtt settings page
