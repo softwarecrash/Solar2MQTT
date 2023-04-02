66273539c4e87621405e00bcb28b22b2b3da4364
softwarecrash <lacrimosa2k2@gmail.com>
2023-04-02 23:39:50 +0200

    add something

--- src/devices/devicelist.h
+++ src/devices/devicelist.h
@@ -1,1 +1,4 @@
-const char PIP30MAX[] PROGMEM = R"=====(#include "PIP30MAX.json")=====";
+//minify it with https://jsonformatter.org/
+const char PIP30MAX[] PROGMEM = R"=====(
+{"name":"PIP30MAX","communication":{"speed":"2400","startByte":"(","separator":" "},"Commands":{"dynamic":{"QMOD":{"Operation_mode":""},"QPIGS":{"Grid_voltage":0,"Grid_frequency":0,"AC_output_voltage":0,"AC_output_frequency":0}},"static":{"QPI":false,"QPIRI":{"Grid_rating_voltage":0,"Grid_rating_current":0,"AC_output_rating_voltage":0,"AC_output_rating_frequency":0,"AC_output_rating_current":0,"AC_output_rating_apparent_power":0,"AC_output_rating_active_power":0,"Battery_rating_voltage":0,"Battery_re-charge_voltage":0,"Battery_under_voltage":0}}}}
+)=====";
