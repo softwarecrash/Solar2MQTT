cd6d52dcf79f9310e41ac7ac064e361a7907c38e
SoftWareCrash <lacrimosa2k2@gmail.com>
2023-05-06 20:40:16 +0200

    add file

---
+++ src/webpages/reboot.h
@@ -0,0 +1,10 @@
+const char HTML_REBOOT[] PROGMEM = R"rawliteral(
+    %HEAD_TEMPLATE%
+<figure class="text-center"><h1>Reboot</h1></figure>
+<div class="d-grid gap-2">
+<a class="btn btn-primary" href="/" role="button">Main</a>
+</div>
+%FOOT_TEMPLATE%
+)rawliteral";
+
+
