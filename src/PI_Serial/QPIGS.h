b50da87da85900a8a103a656eeff01c3d6605c58
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-04 18:33:51 +0200

    add pi18 qpigs

--- src/PI_Serial/QPIGS.h
+++ src/PI_Serial/QPIGS.h
@@ -92,34 +92,36 @@ static const char *const qallList[] = {
     "Fault_code",                // SS
 };
 
-static const char *const P005GS[][8] = {
-    {"AC_in_Voltage", "10"},   // AAAA
-    {"AC_in_Frequenz", "10"},  // BBB
-    {"AC_out_Voltage", "10"},  // CCCC
-    {"AC_out_Frequenz", "10"}, // DDD
-    {"AC_out_VA", "0"},        // EEEE
-    {"AC_out_Watt", "0"},      // FFFF
-    {"AC_out_percent", "0"},   // GGGG
-    {"Battery_Voltage", "10"} //HHHH
-/*
-    "Inverter_Bus_Voltage",           // III
-    
-    "Battery_Charge_Current",         // KKK
-    "Battery_Percent",                // OOO
-    "Inverter_Bus_Temperature",       // TTTT
-    "PV_Input_Current",               // EE.E
-    "PV_Input_Voltage",               // UUU.U
-    "Battery_SCC_Volt",               // WW.WW
-    "Battery_Discharge_Current",      // PPPP
-    "Status_Flag",                    // b0-b7
-    "Battery_voltage_offset_fans_on", // QQ
-    "EEPROM_Version",                 // VV
-    "PV_Charging_Power",              // MMMM
-    "Device_Status",                  // b8-b10
-    "Solar_feed_to_Grid_status",      // Y
-    "Country",                        // ZZ
-    "Solar_feed_to_grid_power",       // AAAA
-    */
+static const char *const P005GS[][28] = {
+    {"AC_in_Voltage", "10"},            // AAAA
+    {"AC_in_Frequenz", "10"},           // BBB
+    {"AC_out_Voltage", "10"},           // CCCC
+    {"AC_out_Frequenz", "10"},          // DDD
+    {"AC_out_VA", "0"},                 // EEEE
+    {"AC_out_Watt", "0"},               // FFFF
+    {"AC_out_percent", "0"},            // GGGG
+    {"Battery_Voltage", "10"},          // HHHH
+    {"Battery_SCC_Volt", "10"},         // III
+    {"Battery_SCC2_Volt", "10"},        // JJJ
+    {"Battery_Discharge_Current", "0"}, // KKK
+    {"Battery_Charge_Current", "0"},    // LLL
+    {"Battery_Percent", "0"},           // MMM
+    {"Inverter_Bus_Temperature", "0"},  // NNN
+    {"MPPT1_Charger_Temperature", "0"}, // OOO
+    {"MPPT2_Charger_Temperature", "0"}, // PPP
+    {"PV_Input_Power", "0"},            // QQQQ
+    {"PV2_Input_Power", "0"},           // RRRR
+    {"PV_Input_Voltage", "10"},         // SSSS
+    {"PV2_Input_Voltage", "10"},        // TTTT
+    {"Configuration_State", "0"},       // U
+    {"MPPT_Charger_Status", "0"},       // V
+    {"MPPT2_CHarger_Status", "0"},      // W
+    {"Load_Connection", "0"},           // X
+    {"Battery_Power_Direction", "0"},   // Y
+    {"ACDC_Power_Direction", "0"},      // Z
+    {"Line_Power_DIrection", "0"},      // a
+    {"Local_Parallel_ID", "0"},         // b
+
 };
 
 bool PI_Serial::PIXX_QPIGS()
@@ -263,40 +265,8 @@ bool PI_Serial::PIXX_QPIGS()
         }
       }
       // make some things pretty
-      // liveData["Battery_Load"] = (liveData["Battery_Charge_Current"].as<unsigned short>() - liveData["Battery_Discharge_Current"].as<unsigned short>());
-      // liveData["PV_Input_Power"] = (liveData["PV_Input_Voltage"].as<unsigned short>() * liveData["PV_Input_Current"].as<unsigned short>());
+      liveData["Battery_Load"] = (liveData["Battery_Charge_Current"].as<unsigned short>() - liveData["Battery_Discharge_Current"].as<unsigned short>());
     }
-
-    /*
-        if (get.raw.qall != "NAK" || get.raw.qall != "ERCRC")
-        {
-          String strsQALL[30];
-          //  Split the string into substrings
-          int StringCountQALL = 0;
-          while (commandAnswerQALL.length() > 0)
-          {
-            int index = commandAnswerQALL.indexOf(delimiter);
-            if (index == -1) // No space found
-            {
-              strsQALL[StringCountQALL++] = commandAnswerQALL;
-              break;
-            }
-            else
-            {
-              strsQALL[StringCountQALL++] = commandAnswerQALL.substring(0, index);
-              commandAnswerQALL = commandAnswerQALL.substring(index + 1);
-            }
-          }
-
-          for (unsigned int i = 0; i < sizeof qallList / sizeof qallList[0]; i++)
-          {
-            if (!strsQALL[i].isEmpty() && strcmp(qallList[i], "") != 0)
-              liveData[qallList[i]] = (int)(strsQALL[i].toFloat() * 100 + 0.5) / 100.0;
-          }
-          liveData["Inverter_Operation_Mode"] = getModeDesc((char)liveData["Inverter_Operation_Mode"].as<String>().charAt(0));
-          liveData["Battery_Load"] = (liveData["Battery_Charge_Current"].as<unsigned short>() - liveData["Battery_Discharge_Current"].as<unsigned short>());
-        }
-        */
     return true;
   }
   else if (protocol == NoD)
