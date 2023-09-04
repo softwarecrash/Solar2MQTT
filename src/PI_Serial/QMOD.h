1494d3e66f951038dd108198415ab080d882b458
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-04 19:52:37 +0200

    changes

--- src/PI_Serial/QMOD.h
+++ src/PI_Serial/QMOD.h
@@ -1,18 +1,47 @@
 bool PI_Serial::PIXX_QMOD()
 {
-  String commandAnswer = this->requestData("QMOD");
-  get.raw.qmod = commandAnswer;
-    if (commandAnswer == "NAK")
+  if (protocol == PI30)
   {
+    String commandAnswer = this->requestData("QMOD");
+    get.raw.qmod = commandAnswer;
+    if (commandAnswer == "NAK")
+    {
+      return true;
+    }
+    if (commandAnswer == "ERCRC")
+    {
+      return false;
+    }
+    if (commandAnswer.length() == 1)
+    {
+      liveData["Inverter_Operation_Mode"] = getModeDesc((char)commandAnswer.charAt(0));
+    }
     return true;
   }
-    if(commandAnswer == "ERCRC")
+  else if (protocol == PI18)
+  {
+    String commandAnswer = this->requestData("^P006MOD");
+    get.raw.qmod = commandAnswer;
+    if (commandAnswer == "NAK")
+    {
+      return true;
+    }
+    if (commandAnswer == "ERCRC")
+    {
+      return false;
+    }
+    if (commandAnswer.length() == 1)
+    {
+      // liveData["Inverter_Operation_Mode"] = getModeDesc((char)commandAnswer.charAt(0));
+    }
+    return true;
+  }
+  else if (protocol == NoD)
   {
     return false;
   }
-  if (commandAnswer.length() == 1)
+  else
   {
-    liveData["Inverter_Operation_Mode"] = getModeDesc((char)commandAnswer.charAt(0));
+    return false;
   }
-  return true;
 }
