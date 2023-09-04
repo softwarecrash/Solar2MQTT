0410469c61d5947c66949b7cee913146e0f33fdf
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-04 19:56:51 +0200

    push

--- src/PI_Serial/QMOD.h
+++ src/PI_Serial/QMOD.h
@@ -30,9 +30,33 @@ bool PI_Serial::PIXX_QMOD()
     {
       return false;
     }
-    if (commandAnswer.length() == 1)
+    if (commandAnswer.length() == 2)
     {
-      // liveData["Inverter_Operation_Mode"] = getModeDesc((char)commandAnswer.charAt(0));
+      switch (commandAnswer.toInt())
+      {
+      case 0:
+        liveData["Inverter_Operation_Mode"] = "Power on";
+        break;
+      case 1:
+        liveData["Inverter_Operation_Mode"] = "Standby";
+        break;
+      case 2:
+        liveData["Inverter_Operation_Mode"] = "BYpass";
+        break;
+      case 3:
+        liveData["Inverter_Operation_Mode"] = "Battery";
+        break;
+      case 4:
+        liveData["Inverter_Operation_Mode"] = "Fault";
+        break;
+      case 5:
+        liveData["Inverter_Operation_Mode"] = "Hybrid";
+        break;
+      default:
+        liveData["Inverter_Operation_Mode"] = "No data";
+        break;
+      }
+
     }
     return true;
   }
