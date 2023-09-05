a54fe56921069f5f1d5eda1a168a2ccbe7d0ec05
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-05 10:08:58 +0200

    changes

--- src/PI_Serial/QEX.h
+++ src/PI_Serial/QEX.h
@@ -86,6 +86,87 @@ bool PI_Serial::PIXX_QEX()
   }
   else if (protocol == PI18)
   {
+    String commandAnswer = this->requestData("^P005ET");
+    get.raw.qet = commandAnswer;
+    if (commandAnswer == "NAK")
+    {
+      return true;
+    }
+    if (commandAnswer == "ERCRC")
+    {
+      return false;
+    }
+    liveData["PV_generation_sum"] = commandAnswer;
+    return true;
+
+    commandAnswer = this->requestData("^P004T");
+    get.raw.qt = commandAnswer;
+    if (commandAnswer == "ERCRC")
+    {
+      return false;
+    }
+    if (commandAnswer == "NAK")
+    {
+      return true;
+    }
+    else
+    {
+      commandAnswer = this->requestData("QEY" + get.raw.qt.substring(0, 4));
+      get.raw.qey = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["PV_generation_year"] = commandAnswer;
+
+      commandAnswer = this->requestData("QEM" + get.raw.qt.substring(0, 6));
+      get.raw.qem = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["PV_generation_month"] = commandAnswer;
+
+      commandAnswer = this->requestData("QED" + get.raw.qt.substring(0, 8));
+      get.raw.qed = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["PV_generation_day"] = commandAnswer;
+
+      commandAnswer = this->requestData("QLT");
+      get.raw.qlt = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["AC_in_generation_sum "] = commandAnswer;
+
+      commandAnswer = this->requestData("QLY" + get.raw.qt.substring(0, 4));
+      get.raw.qly = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["AC_in_generation_year"] = commandAnswer;
+
+      commandAnswer = this->requestData("QLM" + get.raw.qt.substring(0, 6));
+      get.raw.qlm = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["AC_in_generation_month"] = commandAnswer;
+
+      commandAnswer = this->requestData("QLD" + get.raw.qt.substring(0, 8));
+      get.raw.qld = commandAnswer;
+      if (commandAnswer == "ERCRC")
+        return false;
+      if (commandAnswer == "NAK")
+        return true;
+      liveData["AC_in_generation_day"] = commandAnswer;
+    }
     return true;
   }
   else if (protocol == NoD)
