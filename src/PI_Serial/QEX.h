4ec5021fe4bda9caadcec7898a827221c2ce8828
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-05 10:18:42 +0200

    changes

--- src/PI_Serial/QEX.h
+++ src/PI_Serial/QEX.h
@@ -111,7 +111,7 @@ bool PI_Serial::PIXX_QEX()
     }
     else
     {
-      commandAnswer = this->requestData("QEY" + get.raw.qt.substring(0, 4));
+      commandAnswer = this->requestData("^P009EY" + get.raw.qt.substring(0, 4));
       get.raw.qey = commandAnswer;
       if (commandAnswer == "ERCRC")
         return false;
@@ -119,7 +119,7 @@ bool PI_Serial::PIXX_QEX()
         return true;
       liveData["PV_generation_year"] = commandAnswer;
 
-      commandAnswer = this->requestData("QEM" + get.raw.qt.substring(0, 6));
+      commandAnswer = this->requestData("^P011EM" + get.raw.qt.substring(0, 6));
       get.raw.qem = commandAnswer;
       if (commandAnswer == "ERCRC")
         return false;
@@ -127,46 +127,13 @@ bool PI_Serial::PIXX_QEX()
         return true;
       liveData["PV_generation_month"] = commandAnswer;
 
-      commandAnswer = this->requestData("QED" + get.raw.qt.substring(0, 8));
+      commandAnswer = this->requestData("^P013ED" + get.raw.qt.substring(0, 8));
       get.raw.qed = commandAnswer;
       if (commandAnswer == "ERCRC")
         return false;
       if (commandAnswer == "NAK")
         return true;
       liveData["PV_generation_day"] = commandAnswer;
-
-      commandAnswer = this->requestData("QLT");
-      get.raw.qlt = commandAnswer;
-      if (commandAnswer == "ERCRC")
-        return false;
-      if (commandAnswer == "NAK")
-        return true;
-      liveData["AC_in_generation_sum "] = commandAnswer;
-
-      commandAnswer = this->requestData("QLY" + get.raw.qt.substring(0, 4));
-      get.raw.qly = commandAnswer;
-      if (commandAnswer == "ERCRC")
-        return false;
-      if (commandAnswer == "NAK")
-        return true;
-      liveData["AC_in_generation_year"] = commandAnswer;
-
-      commandAnswer = this->requestData("QLM" + get.raw.qt.substring(0, 6));
-      get.raw.qlm = commandAnswer;
-      if (commandAnswer == "ERCRC")
-        return false;
-      if (commandAnswer == "NAK")
-        return true;
-      liveData["AC_in_generation_month"] = commandAnswer;
-
-      commandAnswer = this->requestData("QLD" + get.raw.qt.substring(0, 8));
-      get.raw.qld = commandAnswer;
-      if (commandAnswer == "ERCRC")
-        return false;
-      if (commandAnswer == "NAK")
-        return true;
-      liveData["AC_in_generation_day"] = commandAnswer;
-
 
       return true;
     }
