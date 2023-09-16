5217aa8ff571f8ddb2da9d234738d850254f2258
softwarecrash <lacrimosa2k2@gmail.com>
2023-09-16 09:50:23 +0200

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -581,7 +581,6 @@ bool sendtoMQTT()
       {
         char msgBuffer1[200];
         sprintf(msgBuffer1, "%s/%s/%s", settings.data.mqttTopic, jsonDev.key().c_str(), jsondat.key().c_str());
-        // DEBUG_PRINTLN(msgBuffer1);
         mqttclient.publish(msgBuffer1, jsondat.value().as<String>().c_str());
       }
     }
