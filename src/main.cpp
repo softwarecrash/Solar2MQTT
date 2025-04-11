ac33eefa3bd73e7f9f3eaee6fdfa8c97d1138f17
SoftWareCrash <lacrimosa2k2@gmail.com>
2025-04-11 14:04:02 +0200

    correct Ap manager settings

--- src/main.cpp
+++ src/main.cpp
@@ -176,9 +176,10 @@ void setup()
   AsyncWiFiManager wm(&server, &dns);
   sprintf(mqttClientId, "%s-%06X", settings.data.deviceName, ESP.getChipId());
 
-  wm.setMinimumSignalQuality(20); // filter weak wifi signals
-  // wm.setConnectTimeout(15);       // how long to try to connect for before continuing
-  // wm.setConfigPortalTimeout(120); // auto close configportal after n seconds
+  wm.setDebugOutput(false);       // disable wifimanager debug output
+  wm.setMinimumSignalQuality(25); // filter weak wifi signals
+  wm.setConnectTimeout(10);       // how long to try to connect for before continuing
+  wm.setConfigPortalTimeout(300); // auto close configportal after n seconds
   wm.setSaveConfigCallback(saveConfigCallback);
 /*
   DEBUG_PRINTLN();
